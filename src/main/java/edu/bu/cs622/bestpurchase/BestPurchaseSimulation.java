8f1ebc3b9b41e925ab6d21b31f6a42ea1832321f
Dexter Legaspi <1777690+dalegaspi@users.noreply.github.com>
2022-06-13 10:53:00 -0400

    wip

--- src/main/java/edu/bu/cs622/bestpurchase/BestPurchaseSimulation.java
+++ src/main/java/edu/bu/cs622/bestpurchase/BestPurchaseSimulation.java
@@ -1,10 +1,18 @@
 package edu.bu.cs622.bestpurchase;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
 import edu.bu.cs622.bestpurchase.factories.BestPurchaseFactory;
 import edu.bu.cs622.bestpurchase.factories.DaggerBestPurchaseFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -13,8 +21,35 @@ import java.util.concurrent.CompletableFuture;
 public class BestPurchaseSimulation {
     private static Logger logger = LoggerFactory.getLogger(BestPurchaseSimulation.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         logger.info("BestPurchase Add Item to Cart Use Case Simulation begins.");
+
+        // Setup terminal and screen layers
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        Screen screen = new TerminalScreen(terminal);
+        screen.startScreen();
+
+        // Create panel to hold components
+        Panel panel = new Panel();
+        panel.setLayoutManager(new GridLayout(2));
+
+        panel.addComponent(new Label("Forename"));
+        panel.addComponent(new TextBox());
+
+        panel.addComponent(new Label("Surname"));
+        panel.addComponent(new TextBox());
+
+        panel.addComponent(new EmptySpace(new TerminalSize(0,0))); // Empty space underneath labels
+        panel.addComponent(new Button("Submit"));
+
+        // Create window to hold the panel
+        BasicWindow window = new BasicWindow();
+        window.setComponent(panel);
+
+        // Create gui and start gui
+        MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.BLUE));
+        gui.addWindowAndWait(window);
+
         BestPurchaseFactory factory = DaggerBestPurchaseFactory.create();
 
         var shopper = factory.buildShopper();
